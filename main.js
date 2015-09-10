0329474739cdb736188b7a70f60a119c9da84437
Jeff H <jahuangi@yahoo.ca>
2015-09-09 17:12:48 -0700

    part3 bonus complete. Hover over song name reveals song.

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 
 $(document).ready(function() {
 
-  if (!songQueue) {
-    var songQueue     = [];
-    var songNameQueue = [];
-  };
+  var songQueue     = [];
   // Your code here...
   // Let user enter song
   // On submit event, we will read the input song
@@ -13,12 +10,16 @@ $(document).ready(function() {
 
   var jukebox = function() {
     if (songQueue.length > 0) {
-      var songName = songNameQueue.shift();
-      var songStr = songQueue.shift();
+      var songObj   = songQueue.shift();
+      var songName  = songObj.name;
+      var songStr   = songObj.song;
       $("#now-playing").html("Playing " + songName);
       console.log("Playing song: " + songStr);
+      $("#" + songName).off('mouseenter');
+      $("#" + songName).off('mouseleave');
       playSong(parseSong(songStr),500,jukebox);
-      $("#song-queue").children().first().remove();
+      
+      $("#song-body").children().first().remove();
     } else {
       $("#now-playing").html("");
     };
@@ -31,19 +32,33 @@ $(document).ready(function() {
   });
 
   $("#song-form").on('submit', function(event) {
-    console.log("Submit Event");
     var newSongName = $("input[name='song-name']").eq(0).val().trim();
     var newSong     = $("input[name='notes']").eq(0).val().trim();
     //Clear textbox
     $("input[name='song-name']").eq(0).val("");
     $("input[name='notes']").eq(0).val("");
+
     console.log("NewSong: " + newSong);
-    $("#song-queue").append("<li>" + newSongName + ": " + newSong + "</li>");
-    songNameQueue.push(newSongName);
-    songQueue.push(newSong); 
+    //$("#song-queue").append("<li>" + newSongName + ": " + newSong + "</li>");
+    var nameTag = "<td id='" + newSongName + "'>" + newSongName + "</td>";
+    var songTag = "<td id='" + newSongName + "-song' hidden>" + newSong + "</td>";
+    $("#song-body").append("<tr>" + nameTag + songTag + "</tr>");
+    songQueue.push({name: newSongName, song: newSong});
     console.log(songQueue);
+    //Add a handler for this new song.
+    $("#" + newSongName).on('mouseenter',function(event) {
+      $("#" + newSongName + "-song").attr("hidden",false);
+    });
+    $("#" + newSongName).on('mouseleave',function(event) {
+      $("#" + newSongName + "-song").attr("hidden",true);
+    });
     event.preventDefault();
   });
 
+  $("#songQueue").on('mouseenter',function(event){
+    for (var i=0; i<songQueue.length; i++) {
+    };
+    
+  });
 
 });
